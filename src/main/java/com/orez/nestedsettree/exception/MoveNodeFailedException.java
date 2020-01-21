944bea1405d93081acee7cd9e00a00cb7b9d3167
OrenZiv <oren.ziv@debijenkorf.nl>
2020-01-21 18:13:36 +0100

    Changed return status for delete and move errors

--- src/main/java/com/orez/nestedsettree/exception/MoveNodeFailedException.java
+++ src/main/java/com/orez/nestedsettree/exception/MoveNodeFailedException.java
@@ -3,7 +3,7 @@ package com.orez.nestedsettree.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class MoveNodeFailedException extends RuntimeException {
 
     public MoveNodeFailedException() {
